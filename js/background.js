d766296edfa96ee622c4bcf86db1027376eb7109
JongSeob <cjsgkwkd4@gmail.com>
2022-03-14 18:08:38 +0900

    complete

--- js/background.js
+++ js/background.js
@@ -1,5 +1,4 @@
-// const images = ["0.jpg", "1.jpg", "2.jpg"];
-const images = ["0.jpg"];
+const images = ["0.jpg", "1.jpg", "2.jpg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const body = document.querySelector("body");
